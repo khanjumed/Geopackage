c640c98001a79a63a044c64eaf140cb58ce4154b
Jumed Khan <jumed.khan@plainsurf.com>
2025-07-24 20:14:39 +0530

    public alter to config

--- public.go
+++ public.go
@@ -1,10 +1,21 @@
 package Geopackage
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/khanjumed/geopackage/internal/config" // Import the config package
 	"github.com/khanjumed/geopackage/internal/routes"
 )
 
+// RegisterAllRoutes initializes the DB and registers all the routes
 func RegisterAllRoutes(r *gin.Engine) {
+	// Initialize the DB connectionc
+	err := config.InitDB()
+	if err != nil {
+		log.Fatalf("Failed to initialize DB: %v", err) // Stop the app if DB init fails
+	}
+
+	// Register all the routes
 	routes.Register(r)
 }
